bf719334ede01e13405b65e53dfcad2dd7d529f7
Julien Massot <julien.massot@iot.bzh>
2021-04-13 08:08:34 +0200

    drivers: timer: rcar-cmt synchronize with upstream branch
    - fix timer api breaks
    - rename rcar,cmt into renesas,rcar-cmt
    - use a second channel to enable busy wait in isr context
    
    Signed-off-by: Julien Massot <julien.massot@iot.bzh>

--- drivers/timer/rcar_cmt_timer.c
+++ drivers/timer/rcar_cmt_timer.c
@@ -9,17 +9,16 @@
 #include <drivers/clock_control.h>
 #include <drivers/clock_control/rcar_clock_control.h>
 
-#define DT_DRV_COMPAT rcar_cmt
+#define DT_DRV_COMPAT renesas_rcar_cmt
 
-#define TIMER_IRQ		DT_INST_IRQN(0)
-#define TIMER_BASE_ADDR		DT_INST_REG_ADDR(0)
-#define TIMER_CLOCK_FREQUENCY	DT_INST_PROP(0, clock_frequency)
+#define TIMER_IRQ              DT_INST_IRQN(0)
+#define TIMER_BASE_ADDR        DT_INST_REG_ADDR(0)
+#define TIMER_CLOCK_FREQUENCY  DT_INST_PROP(0, clock_frequency)
 
-#define CLOCK_SUBSYS            DT_INST_CLOCKS_CELL(0, module)
+#define CLOCK_SUBSYS           DT_INST_CLOCKS_CELL(0, module)
 
-#define CYCLES_PER_SEC		TIMER_CLOCK_FREQUENCY
-#define CYCLES_PER_MS           CYCLES_PER_SEC / 1000
-#define CYCLES_PER_TICK		(CYCLES_PER_SEC / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
+#define CYCLES_PER_SEC         TIMER_CLOCK_FREQUENCY
+#define CYCLES_PER_TICK        (CYCLES_PER_SEC / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
 
 static struct rcar_cpg_clk mod_clk = {
 	.module = DT_INST_CLOCKS_CELL(0, module),
@@ -29,48 +28,57 @@ static struct rcar_cpg_clk mod_clk = {
 BUILD_ASSERT(CYCLES_PER_TICK > 1,
 	     "CYCLES_PER_TICK must be greater than 1");
 
-#define CMCOR0_OFFSET	0x018	/* Compare match timer constant register 0 */
-#define CMCNT0_OFFSET	0x014	/* Compare match timer counter 0 */
-#define CMCSR0_OFFSET	0x010	/* Compare match timer control/status register 0 */
+#define CMCOR0_OFFSET                   0x018   /* constant register 0 */
+#define CMCNT0_OFFSET                   0x014   /* counter 0 */
+#define CMCSR0_OFFSET                   0x010   /* control/status register 0 */
 
-#define CMCLKE		0xB00	/* CLK enable register */
-#define CLKEN		BIT(5)
+#define CMCOR1_OFFSET                   0x118   /* constant register 1 */
+#define CMCNT1_OFFSET                   0x114   /* counter 1 */
+#define CMCSR1_OFFSET                   0x110   /* control/status register 1 */
 
-#define CMSTR0_OFFSET	0x000	/* Compare match timer start register 0 */
-#define START_BIT	BIT(0)
+#define CMCLKE                          0xB00   /* CLK enable register */
+#define CLKEN0                          BIT(5)  /* Enable Clock for channel 0 */
+#define CLKEN1                          BIT(6)  /* Enable Clock for channel 1 */
 
-#define	CSR_CLK_DIV_1			0x00000007
-#define	CSR_ENABLE_COUNTER_IN_DEBUG	BIT(3)
-#define	CSR_ENABLE_INTERRUPT		BIT(5)
-#define	CSR_FREE_RUN			BIT(8)
-#define	CSR_WRITE_FLAG			BIT(13)
-#define	CSR_OVERFLOW_FLAG		BIT(14)
-#define	CSR_MATCH_FLAG			BIT(15)
+#define CMSTR0_OFFSET                   0x000   /* Timer start register 0 */
+#define CMSTR1_OFFSET                   0x100   /* Timer start register 1 */
+#define START_BIT                       BIT(0)
 
-static uint32_t cycle_count;
+#define CSR_CLK_DIV_1                   0x00000007
+#define CSR_ENABLE_COUNTER_IN_DEBUG     BIT(3)
+#define CSR_ENABLE_INTERRUPT            BIT(5)
+#define CSR_FREE_RUN                    BIT(8)
+#define CSR_WRITE_FLAG                  BIT(13)
+#define CSR_OVERFLOW_FLAG               BIT(14)
+#define CSR_MATCH_FLAG                  BIT(15)
 
 static void cmt_isr(void *arg)
 {
 	ARG_UNUSED(arg);
 	uint32_t reg_val;
 
-	/* Read the interrupt status, then write it back to clear the interrupt */
+	/* clear the interrupt */
 	reg_val = sys_read32(TIMER_BASE_ADDR + CMCSR0_OFFSET);
 	reg_val &= ~CSR_MATCH_FLAG;
 	sys_write32(reg_val, TIMER_BASE_ADDR + CMCSR0_OFFSET);
 
-	cycle_count += CYCLES_PER_TICK;
-
-	/* Announce to the kernel*/
-	z_clock_announce(1);
+	/* Announce to the kernel */
+	sys_clock_announce(1);
 }
 
-int z_clock_driver_init(const struct device *device)
+/*
+ * Initialize both channels at same frequency,
+ * Set the first one to generates interrupt at CYCLES_PER_TICK.
+ * The second one is used for cycles count, the match value is set
+ * at max uint32_t.
+ */
+int sys_clock_driver_init(const struct device *device)
 {
 	const struct device *clk;
 	uint32_t reg_val;
 	int i, ret;
 
+	ARG_UNUSED(device);
 	clk = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(0));
 	if (!clk) {
 		return -ENODEV;
@@ -81,30 +89,38 @@ int z_clock_driver_init(const struct device *device)
 		return ret;
 	}
 
-	/* Clock Enable */
-	reg_val = sys_read32(TIMER_BASE_ADDR + CMCLKE);
-	reg_val |= CLKEN;
-	sys_write32(reg_val, TIMER_BASE_ADDR + CMCLKE);
+	/* Supply clock for both channels */
+	sys_write32(CLKEN0|CLKEN1, TIMER_BASE_ADDR + CMCLKE);
 
-	/* Stop the timer */
+	/* Stop both channels */
 	reg_val = sys_read32(TIMER_BASE_ADDR + CMSTR0_OFFSET);
 	reg_val &= ~START_BIT;
 	sys_write32(reg_val, TIMER_BASE_ADDR + CMSTR0_OFFSET);
 
-	/* Set the timer as 32-bit, with RCLK/1 clock */
-	reg_val = CSR_FREE_RUN;
-	reg_val |= CSR_ENABLE_INTERRUPT;
-	reg_val |= CSR_CLK_DIV_1;
-	sys_write32(reg_val, TIMER_BASE_ADDR + CMCSR0_OFFSET);
+	reg_val = sys_read32(TIMER_BASE_ADDR + CMSTR1_OFFSET);
+	reg_val &= ~START_BIT;
+	sys_write32(reg_val, TIMER_BASE_ADDR + CMSTR1_OFFSET);
 
+	/* Set the timers as 32-bit, with RCLK/1 clock */
+	sys_write32(CSR_FREE_RUN | CSR_CLK_DIV_1 | CSR_ENABLE_INTERRUPT,
+		    TIMER_BASE_ADDR + CMCSR0_OFFSET);
+
+	/* Do not enable interrupts for the second channel */
+	sys_write32(CSR_FREE_RUN | CSR_CLK_DIV_1,
+		    TIMER_BASE_ADDR + CMCSR1_OFFSET);
+
+	/* Set the first channel match to CYCLES Per tick*/
 	sys_write32(CYCLES_PER_TICK, TIMER_BASE_ADDR + CMCOR0_OFFSET);
 
-	/* Reset the counter, check WRFLG first */
+	/* Set the second channel match to max uint32 */
+	sys_write32(0xffffffff, TIMER_BASE_ADDR + CMCOR1_OFFSET);
+
+	/* Reset the counter for first channel, check WRFLG first */
 	while (sys_read32(TIMER_BASE_ADDR + CMCSR0_OFFSET) & CSR_WRITE_FLAG)
 		;
 	sys_write32(0, TIMER_BASE_ADDR + CMCNT0_OFFSET);
 
-	for (i = 0; i < 100; i++) {
+	for (i = 0; i < 1000; i++) {
 		if (!sys_read32(TIMER_BASE_ADDR + CMCNT0_OFFSET))
 			break;
 	}
@@ -112,25 +128,24 @@ int z_clock_driver_init(const struct device *device)
 	__ASSERT(sys_read32(TIMER_BASE_ADDR + CMCNT0_OFFSET) == 0,
 			"Fail to clear CMCNT0 register");
 
-	/* Connect timer interrupt */
+	/* Connect timer interrupt for channel 0*/
 	IRQ_CONNECT(TIMER_IRQ, 0, cmt_isr, 0, 0);
 	irq_enable(TIMER_IRQ);
 
-	/* Start the timer */
-	reg_val = sys_read32(TIMER_BASE_ADDR + CMSTR0_OFFSET);
-	reg_val |= START_BIT;
-	sys_write32(reg_val, TIMER_BASE_ADDR + CMSTR0_OFFSET);
+	/* Start the timers */
+	sys_write32(START_BIT, TIMER_BASE_ADDR + CMSTR0_OFFSET);
+	sys_write32(START_BIT, TIMER_BASE_ADDR + CMSTR1_OFFSET);
 
 	return 0;
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t sys_clock_elapsed(void)
 {
 	/* Always return 0 for tickful operation */
 	return 0;
 }
 
-uint32_t z_timer_cycle_get_32(void)
+uint32_t sys_clock_cycle_get_32(void)
 {
-	return cycle_count;
+	return sys_read32(TIMER_BASE_ADDR + CMCNT1_OFFSET);
 }

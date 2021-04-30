8ca02aadef01dba4adebe4879e42336a2a201ab4
Julien Massot <julien.massot@iot.bzh>
2021-04-30 14:35:20 +0200

    wip quick and dirty test for mpu

--- soc/arm/renesas_rcar/gen3/soc.c
+++ soc/arm/renesas_rcar/gen3/soc.c
@@ -8,6 +8,37 @@
 #include <kernel.h>
 #include <device.h>
 #include <init.h>
+#include <arch/arm/aarch32/mpu/arm_mpu.h>
+
+#define MPUTYPE_READ_ONLY		{ .rasr = (P_RO_U_RO_Msk   | (7 << MPU_RASR_TEX_Pos) | MPU_RASR_C_Msk | MPU_RASR_B_Msk | MPU_RASR_XN_Msk) }
+#define MPUTYPE_READ_AND_EXEC    	{ .rasr = (P_RO_U_RO_Msk   | (7 << MPU_RASR_TEX_Pos) | MPU_RASR_C_Msk | MPU_RASR_B_Msk)                   }
+#define MPUTYPE_WBWACACHE		{ .rasr = (P_RW_U_RO_Msk   | (5 << MPU_RASR_TEX_Pos) |                  MPU_RASR_B_Msk)                   }
+#define MPUTYPE_PRIV_DEVICE		{ .rasr = (P_RW_U_NA_Msk   | (2 << MPU_RASR_TEX_Pos))                                                     }
+#define MPUTYPE_DEVICE			{ .rasr = (FULL_ACCESS_Msk | (2 << MPU_RASR_TEX_Pos))                                                     }
+#define MPUTYPE_NO_ACCESS		{ .rasr = (NO_ACCESS_Msk) }
+
+#define MPUTYPE_FULL { .rasr = (FULL_ACCESS_Msk   | (7 << MPU_RASR_TEX_Pos) | MPU_RASR_C_Msk | MPU_RASR_B_Msk)                   }
+
+static const struct arm_mpu_region mpu_regions[] = {
+	MPU_REGION_ENTRY("Default",
+			 0x00000000,
+			 REGION_4G,
+			 MPUTYPE_FULL),
+	MPU_REGION_ENTRY("PERIPHERALS",
+			 0xC0000000,
+			 REGION_1G,
+			 MPUTYPE_DEVICE),
+
+	MPU_REGION_ENTRY("SRAM_1",
+			  0x40000000,
+			 REGION_1G,
+			 MPUTYPE_FULL),
+};
+
+const struct arm_mpu_config mpu_config = {
+	.num_regions = ARRAY_SIZE(mpu_regions),
+	.mpu_regions = mpu_regions,
+};
 
 /**
  *
